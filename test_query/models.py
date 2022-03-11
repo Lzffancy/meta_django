ffeb4010d3c142c143aa00af331f8b4659dfaa4a
jefferli <jefferli@tencent.com>
2022-03-11 22:26:00 +0800

    update git ignore

--- test_query/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here. db_table重写表名
-class Message_board(models.Model):
-    uid = models.CharField(max_length=16, primary_key=True)
-    nick_name = models.CharField(max_length=16)
-    message = models.TextField(max_length=258)
-    created_date = models.DateTimeField(auto_now_add=True)
